360a51c56b7145928ffc49333c0a447eb54b651b
Maikel Wever <maikelwever@gmail.com>
2014-01-09 13:46:05 +0100

    Edit buildscript for changes in submodules

--- buildall.sh
+++ buildall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #buildall.sh
-# Build and Deploy all Jenkins plugins.
+# Build all Jenkins plugins.
 
 set -e
 
 export MAVEN_OPTS="-Dmaven.test.skip=true"
 
-for i in mercurial-plugin Fogbugz AdvancedMercurial FogbugzPlugin GatekeeperPlugin UpmergePlugin
+for i in mercurial-plugin Fogbugz fogbugz-plugin GatekeeperPlugin UpmergePlugin
 do
     pushd $i
         mvn clean:clean
